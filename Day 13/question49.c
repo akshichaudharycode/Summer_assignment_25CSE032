63ee3ee6d06c274b785bcfb236254f29aedf2d62
akshichaudharycode <akshi1953@gmail.com>
2026-06-13 14:54:51 +0530

    Day-13 uploaded

--- Day 13/question49.c
+++ Day 13/question49.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 
-int main() {
-    int n;
+int main()
+{
+    int arr[100], n, i;
 
     printf("Enter size of array: ");
     scanf("%d", &n);
 
-    int arr[n];
-
     printf("Enter %d elements:\n", n);
-    for(int i = 0; i < n; i++) {
+
+    for(i = 0; i < n; i++)
+    {
         scanf("%d", &arr[i]);
     }
 
     printf("Array elements are: ");
-    for(int i = 0; i < n; i++) {
+
+    for(i = 0; i < n; i++)
+    {
         printf("%d ", arr[i]);
     }
 
