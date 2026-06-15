cb0c57a583598ab9a51f62a702892eff8db70bde
akshichaudharycode <akshi1953@gmail.com>
2026-06-15 14:31:41 +0530

    Day-15 uploaded

---
+++ Day 15/question60.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter array elements:\n");
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int index = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        if(arr[i] != 0)
+        {
+            arr[index] = arr[i];
+            index++;
+        }
+    }
+
+    while(index < n)
+    {
+        arr[index] = 0;
+        index++;
+    }
+
+    printf("Array after moving zeroes:\n");
+
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
